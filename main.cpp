8af04dd89247a098e0106690f0f32aa8f5ae6475
Arturo Suarez <manuel.suarez@cimat.mx>
2022-10-20 09:53:05 -0500

    Kernel generation

--- main.cpp
+++ main.cpp
@@ -15,4 +15,18 @@ void FilterCreation(double GKernel[][5])
 
     // sum is for normalization
     double sum = 0.0;
+
+    // generating 5x5 kernel
+    for (int x = -2; x <= 2; x++) {
+        for (int y = -2; y <= 2; y++) {
+            r = sqrt(x * x + y * y);
+            GKernel[x + 2][y + 2] = (exp(-(r * r) / s)) / (M_PI * s);
+            sum += GKernel[x + 2][y + 2];
+        }
+    }
+
+    // normalization
+    for (int i = 0; i < 5; ++i)
+        for (int j = 0; j < 5; ++j)
+            GKernel[i][j] /= sum;
 }
